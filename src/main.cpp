affb9f651ddb2837b29fd5e513d83b3484f828c1
po_chih <55981023+upqsdy46853@users.noreply.github.com>
2022-04-01 06:46:28 +0800

    v0

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include<map>
 #include<vector>
 #include <sys/time.h>
-
 using namespace std;
+
 int m;
 vector<pair<string, bool>> implicant[27][27];
 vector<string> prime_implicant;
@@ -14,7 +14,7 @@ map<int, set<string>> maxterm;
 map<string, set<int>> P;
 vector<string> ans;
 
-double GetTime(void) // get the current time
+double GetTime(void)
 {
     struct timeval tv;
     gettimeofday(&tv, NULL);
@@ -34,7 +34,7 @@ void append(string s, int cnt, int n){
             return;
         }
     }
-    implicant[0][cnt].push_back(pair<string, int>(s, false));
+    implicant[0][cnt].push_back(pair<string, bool>(s, false));
 }
 
 void getSOP(string &s, int cnt, int val){
@@ -57,6 +57,7 @@ void getSOP(string &s, int cnt, int val){
 }
 
 int main(int argc, char* argv[]){
+    //double t = GetTime();
     string line;
     ifstream fptr(argv[1]);
     int _;
@@ -68,6 +69,7 @@ int main(int argc, char* argv[]){
         if(line.size())
             append(line, 0, 0);
     }
+    //delete repeat 
     for(int j=0;j<m;j++){
         std::sort(implicant[0][j].begin(), implicant[0][j].end());
         auto it = std::unique(implicant[0][j].begin(), implicant[0][j].end());
@@ -75,14 +77,10 @@ int main(int argc, char* argv[]){
     }
 
     //primary implicant generation 
-    double t = GetTime();
     for(int i=0;i<=m;i++){
         //get column i
-        //set<string> merged;
         for(int j=0;j<m;j++){
-            //set<string> next_merged;
             for(auto str1=implicant[i][j].begin();str1!=implicant[i][j].end();str1++){
-                //bool merge = false;
                 for(auto str2=implicant[i][j+1].begin();str2!=implicant[i][j+1].end();str2++){
                     int diff_idx = -1;
                     int diff_cnt = 0;
@@ -98,44 +96,30 @@ int main(int argc, char* argv[]){
                         string tmp = (*str1).first;
                         tmp[diff_idx] = '-';
                         implicant[i+1][j].push_back(pair<string ,bool>(tmp, false));
-                        //next_merged.insert(*str2);
-                        //merge = true;
                         (*str1).second = true; 
                         (*str2).second = true; 
                     }
                 }
-                //get prime implicant
-                //if(!merge && merged.find(*str1)==merged.end()){
-                //    prime_implicant.push_back(*str1);
-                //}
             }
+            //get prime implicant
             for(auto p:implicant[i][j]){
                 if(p.second==false){
                     prime_implicant.push_back(p.first);
                 }
             }
+            //delete repeat
             std::sort(implicant[i+1][j].begin(), implicant[i+1][j].end());
             auto it = std::unique(implicant[i+1][j].begin(), implicant[i+1][j].end());
             implicant[i+1][j].resize(distance(implicant[i+1][j].begin(),it));
-            //std::unique(implicant[i+1][j].begin(), implicant[i+1][j].end());
-
-            //merged = next_merged;
             if(i!=0)
                 implicant[i][j].clear();
         }
     }
-    cout<<"Time:"<<GetTime()-t<<endl;
-    //test prime
-    //for(auto i:prime_implicant){
-    //    cout<<i<<endl;
-    //}
-    cout<<prime_implicant.size()<<endl;
 
     //create table
     for(int j=0;j<=m;j++){
         for(auto it=implicant[0][j].begin(); it!=implicant[0][j].end();it++){
             maxterm[stoi((*it).first, 0, 2)] = set<string>();
-            //cout<<*it<<":"<<stoi(*it, 0, 2)<<endl;
         }
     }
 
@@ -147,30 +131,6 @@ int main(int argc, char* argv[]){
     for(auto i:maxterm){
         cnt_set.insert(pair<int, int>(i.second.size(), i.first));
     }
-
-    //test cnt_set
-    //for(auto i:cnt_set){
-    //    cout<<i.first<<","<<i.second<<endl;
-    //}
-
-    //test maxterm
-    //for(auto i:maxterm){
-    //    cout<<i.first<<":";
-    //    cout<<"{";
-    //    for(auto j:i.second)
-    //        cout<<j<<",";
-    //    cout<<"}\n";
-    //}
-
-    //test P
-    //for(auto i:P){
-    //    cout<<i.first<<":";
-    //    cout<<"{";
-    //    for(auto j:i.second)
-    //        cout<<j<<",";
-    //    cout<<"}\n";
-    //}
-
 
     for(auto p:cnt_set){
         if(maxterm.size()==0)
@@ -186,7 +146,6 @@ int main(int argc, char* argv[]){
                     max_str = str;
                 }
             }
-            //string str = *(tmp.begin());
             for(auto col:P[max_str]){
                 for(auto str:maxterm[col]){
                     if(str!=max_str)
@@ -198,7 +157,6 @@ int main(int argc, char* argv[]){
         }
     }
 
-
     int num_literals = 0;
     for(auto str:ans){
         for(auto c:str){
@@ -208,27 +166,12 @@ int main(int argc, char* argv[]){
     }
     ofstream fhdr;
     fhdr.open(argv[2]);
-    fhdr<<num_literals<<endl;
-    fhdr<<ans.size()<<endl;
+    fhdr<<num_literals<<"\n";
+    fhdr<<ans.size()<<"\n";
     for(auto str:ans){
-        fhdr<<str<<endl;
+        fhdr<<str<<"\n";
     }
     fhdr.close();
-    //output
-    //int num_literals = 0;
-    //for(auto str=prime_implicant.begin();str!=prime_implicant.end();str++){
-    //    for(int i=0;i<(*str).size();i++){
-    //        if((*str)[i]=='1'||(*str)[i]=='0')
-    //        num_literals++;
-    //    }
-    //}
-    //ofstream fhdr;
-    //fhdr.open(argv[2]);
-    //fhdr<<num_literals<<endl;
-    //fhdr<<prime_implicant.size()<<endl;
-    //for(auto i=prime_implicant.begin();i!=prime_implicant.end();i++){
-    //    fhdr<<*i<<endl;
-    //}
-    //fhdr.close();
+    //cout<<"Time:"<<GetTime()-t<<"\n";
     return 0;
 }
